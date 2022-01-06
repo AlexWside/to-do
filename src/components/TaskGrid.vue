08b8c05d4f3423912fe169bd950aadd2e6f9c2a7
alexwside <alexsandro.akila@outlook.com>
2022-01-06 14:45:24 -0400

    fist

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="tasks-grid">
+     <template v-if="tasks.length">
+      <Task v-for="task in tasks" :key="task.name"
+      
+      :task="task">
+         
+      </Task>
+     </template>
+     <p v-else class="no-task">Sem tarefas</p>
+   </div>
+</template>
+
+<script>
+import Task from './task.vue'
+export default {
+  components: { Task },
+    props: {
+      tasks: {
+        type:Array, riquired: true
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/task.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="task" 
+  :class="stateClass"
+  >{{task.name}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+     stateClass(){
+       return {
+              pending: this.task.status,
+              done: !this.task.status
+       }
+     }
+  }
+};
+</script>
+
+<style>
+
+.task{
+  box-sizing: border-box;
+  width: 350px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+.pending{
+  background-color: #ff6666;
+  border-right: #ff3c3c solid 5px;
+}
+
+.pending{
+  background-color: #ff6666;
+  border-right: #ff3c3c solid 12px;
+}
+
+.done{
+  color: #ddd;
+  background-color: #70db70;
+  border-right: #33cc33 solid 12px;
+  text-decoration: line-through;
+}
+</style>
